9a157cc1c096b11255ed42fc65f851ac02cba2d0
Aaron Addleman <aaronaddleman@gmail.com>
2016-11-16 21:49:41 -0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,2 +1,6 @@
 brew tap Homebrew/bundle
 brew bundle
+# install python v2.7.12
+CFLAGS="-I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib" pyenv install -v 2.7.12
+# install python v3.5.0
+CFLAGS="-I$(brew --prefix openssl)/include -I$(xcrun --show-sdk-path)/usr/include" LDFLAGS="-L$(brew --prefix openssl)/lib" pyenv install -v 3.5.0
